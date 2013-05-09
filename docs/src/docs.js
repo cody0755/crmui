4ffc5c97ba1b5d3b80fb13d92d2268ca80a41fe9
satans17 <satans17@gmail.com>
2013-05-09 16:15:28 +0800

    crmui首次提交

---
+++ docs/src/docs.js
@@ -0,0 +1,44 @@
+
+
+
+
+		
+		
+KISSY.ready(function(S){
+	// SyntaxHighlighter.config.clipboardSwf = 'src/SyntaxHighlighter/scripts/clipboard.swf';
+	// SyntaxHighlighter.all();
+
+	$1k.highlight.init({    //组件初始化
+        skin:'crm',//默认皮肤
+        edit:true,//开启编辑运行
+        JsRequire:''//导入js
+    });
+	
+
+    KISSY.use("switchable", function (S, Switchable) {
+
+        // S.all(".crm-tabs").each(function (n) {
+            // var t = new Tabs({srcNode: n});
+			// t.render();
+			// console.log(t);
+        // });
+		
+		// var Tabs = Switchable.Tabs
+        // S.all(".crm-tabs").each(function (n) {
+			// var tabs = new Tabs(n, {
+				// triggerType:"click",
+				// triggerCls:"crm-tabs-tab",
+				// panelCls:"crm-tabs-panel",
+				// activeTriggerCls:"crm-tabs-tab-selected",
+				// switchTo : 0
+			// });
+        // });
+		
+
+		
+	
+
+    });
+
+		
+});
---
+++ src/crm.js
@@ -0,0 +1,18 @@
+/**
+ * @name: crmdpl
+ * @overview: crmdpl∞¸≈‰÷√
+ * @creater: changyin@taobao.com
+ */
+
+ 
+KISSY.config({
+    packages:{
+        "crm":{
+            base:'http://x.com/biz/',
+            tag:'x',
+            debug:true
+        }
+    }
+});
+
+KISSY.config("components/prefixCls","crm-");
